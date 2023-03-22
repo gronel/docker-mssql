ad6682b6b0a8861f797d10067eadeac1cc93f5d6
gronel <ronelgonzales5@gmail.com>
2023-03-22 23:57:03 +0800

    update

--- data/mysql/init/01-databases.sql
+++
@@ -1,9 +0,0 @@
-#disable strict mode
-set global sql_mode='';
-
-#create required users
-GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'%' IDENTIFIED BY 'loader';
-GRANT ALL PRIVILEGES ON *.* TO 'dbadmin'@'127.0.0.1' IDENTIFIED BY 'loader';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY '';
-
